e0093fbbdd4e6483f9bf95f831c837e6f9e2b17f
Starkovpro <proff.work.starkov@gmail.com>
2026-03-27 22:18:44 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,26 @@
-const CACHE = 'neyro-shef-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'bestfriend-v1';
+const URLS_TO_CACHE = ['/', '/index.html', '/manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('/api/')) return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+self.addEventListener('fetch', (event) => {
+  // Don't cache API calls
+  if (event.request.url.includes('/api/')) return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
